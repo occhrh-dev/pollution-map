f08081cf5870895f2e3e16c5c9a5e844bba2253d
occhrh-dev <occ.hrh@gmail.com>
2026-09-09 09:44:41 +0700

    Connect production Google login backend

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // ค่าทั้งสองเป็นรหัสสาธารณะสำหรับเชื่อมบริการ ไม่ใช่รหัสผ่านหรือกุญแจลับ
 // ห้ามใส่ Client Secret, รหัสผ่าน, access token หรือ service-account key ในไฟล์นี้
 window.POLLUTION_MAP_CONFIG = {
-  googleClientId: '',
-  appsScriptWebAppUrl: ''
+  googleClientId: '673470299265-c9qkpkmgmimnt59e7vtcfnbudb1njvo9.apps.googleusercontent.com',
+  appsScriptWebAppUrl: 'https://script.google.com/macros/s/AKfycbx81qy4KrwkVAQ0GdVMYJDraZVxDjsz4ZqYeBwXNtvaVXoURQxfK5VVAwRKtpNxeCQO3A/exec'
 };
